2524d36016f51da46f42c462bae0a81d68ca303c
luksinocencio <luksinocencio@gmail.com>
2025-12-09 21:41:27 -0300

    liveActivity-iOS settings preview

--- section08_live_activity/app/ios/NotificationWidget/NotificationWidget.swift
+++ section08_live_activity/app/ios/NotificationWidget/NotificationWidget.swift
@@ -21,6 +21,12 @@ struct NotificationWidgetView: Widget {
         DynamicIslandExpandedRegion(.leading, priority: 1) {
           Text("Dynamic Island")
         }
+        DynamicIslandExpandedRegion(.center, priority: 1) {
+          Text("Dynamic Island")
+        }
+        DynamicIslandExpandedRegion(.trailing, priority: 1) {
+          Text("Dynamic Island")
+        }
       } compactLeading: {
         Text("L")
       } compactTrailing: {
@@ -29,5 +35,25 @@ struct NotificationWidgetView: Widget {
         Text("M")
       }
     }
+  }
+}
+
+struct NotificationWidgetPreviews: PreviewProvider {
+  static let attributes = NotificationAttributes(restaurant: "Burgão do Jão", order: "#0208")
+  static let contentState = NotificationAttributes.ContentState(status: "Recebemos o seu pedido.", description: "Em até 30 minutos seu pedido sai para entrega", step: 1, stepMessage: "Boas notícias!", imageStep: "cooking")
+  
+  static var previews: some View {
+    attributes
+      .previewContext(contentState, viewKind: .content)
+      .previewDisplayName("Notification")
+    attributes
+      .previewContext(contentState,viewKind: .dynamicIsland(.expanded))
+      .previewDevice("Inslaned Expanded")
+    attributes
+      .previewContext(contentState, viewKind: .dynamicIsland(.compact))
+      .previewDevice("Island Compact")
+    attributes
+      .previewContext(contentState, viewKind: .dynamicIsland(.minimal))
+      .previewDevice("Island Minimal")
   }
 }
