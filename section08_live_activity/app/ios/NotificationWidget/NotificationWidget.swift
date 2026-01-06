8f5a90eb47c603c2c64a59a00a342d0be0cbf653
luksinocencio <luksinocencio@gmail.com>
2026-01-06 12:19:40 -0300

    liveActivity-iOS custom notification

--- section08_live_activity/app/ios/NotificationWidget/NotificationWidget.swift
+++ section08_live_activity/app/ios/NotificationWidget/NotificationWidget.swift
@@ -10,12 +10,90 @@ struct NotificationWidget: WidgetBundle {
   }
 }
 
+func hexStringToUiColor (hex: String) -> UIColor {
+  var hexString: String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+  
+  if(hexString.hasPrefix("#")) {
+    hexString.remove(at: hexString.startIndex)
+  }
+  
+  if((hexString.count) != 6){
+    return UIColor.gray
+  }
+  
+  var rgbValue: UInt64 = 0
+  
+  Scanner(string: hexString).scanHexInt64(&rgbValue)
+  
+  return UIColor(
+    red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+    green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+    blue: CGFloat((rgbValue & 0x0000FF)) / 255.0,
+    alpha: CGFloat(1.0)
+  )
+}
+
+struct ProgressBar: View {
+  let activityColor = Color(hexStringToUiColor(hex: "#48EAB6"))
+  let inactiveColor = Color(hexStringToUiColor(hex: "#D3D3D3"))
+  
+  var body: some View {
+    HStack {
+      RoundedRectangle(cornerRadius: 8, style: .continuous)
+        .frame(width: 70, height: 5)
+        .foregroundColor(activityColor)
+      
+      RoundedRectangle(cornerRadius: 8, style: .continuous)
+        .frame(width: 170, height: 5)
+        .foregroundColor(inactiveColor)
+      
+      RoundedRectangle(cornerRadius: 8, style: .continuous)
+        .frame(width: 70, height: 5)
+        .foregroundColor(inactiveColor)
+      
+    }.padding([.top, .bottom], 4)
+  }
+}
+
+struct ContentNotification: View {
+  var body: some View {
+    VStack(alignment: .leading) {
+      HStack() {
+        Image("logoBurgao")
+          .resizable()
+          .frame(width: 50, height: 50)
+        
+        VStack(alignment: .leading) {
+          Text("Burger do Zé")
+            .font(.system(size: 18))
+            .fontWeight(.bold)
+            .foregroundColor(.black)
+          
+          Text("Pedido: **#0208**")
+            .font(.headline)
+            .foregroundColor(.black)
+        }
+      }
+      
+      Text("Estamos preparando o seu burgão")
+        .font(.subheadline)
+        .foregroundColor(.black)
+      
+      ProgressBar()
+      
+      Text("Em até 30 minutos seu pedido sai para entrega")
+        .font(.subheadline)
+        .foregroundColor(.gray)
+    }
+    .frame(maxWidth: .infinity, alignment: .leading)
+    .padding(16)
+  }
+}
+
 struct NotificationWidgetView: Widget {
   var body: some WidgetConfiguration {
     ActivityConfiguration(for: NotificationAttributes.self) { context in
-      VStack() {
-        Text("Principal Notification")
-      }
+      ContentNotification()
     } dynamicIsland: { context in
       DynamicIsland {
         DynamicIslandExpandedRegion(.leading, priority: 1) {
